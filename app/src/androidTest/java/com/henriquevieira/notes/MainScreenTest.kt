b9d98c39e680d34ee97ef87ebafd8541aca826e5
Henrique Vieira <hbaggiovieira@gmail.com>
2022-12-06 16:14:28 -0300

    Improved tests

--- app/src/androidTest/java/com/henriquevieira/notes/MainScreenTest.kt
+++ app/src/androidTest/java/com/henriquevieira/notes/MainScreenTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performTextInput
 import com.henriquevieira.notes.features.main.MainActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -31,6 +32,20 @@ class MainScreenTest {
             .assertIsDisplayed()
             .assertTextEquals("My Notes")
             .assertExists()
+    }
+
+    @Test
+    fun testTextField() {
+        composeTestRule.onNodeWithTag("TEXT_FIELD_TAG")
+            .assertTextEquals("")
+            .assertIsDisplayed()
+
+        composeTestRule.onNodeWithTag("TEXT_FIELD_TAG")
+            .performTextInput("Test text")
+
+        composeTestRule.onNodeWithTag("TEXT_FIELD_TAG")
+            .assertTextEquals("Test text")
+            .assertIsDisplayed()
     }
 
 }
