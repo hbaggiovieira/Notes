e92a3423116797672ae62225b4a99c9d6d990a58
Henrique Vieira <hbaggiovieira@gmail.com>
2022-12-09 15:27:03 -0300

    Update versions

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -6,8 +6,8 @@ object Config {
 
     const val buildToolsVersion = "33.0.1"
 
-    const val versionCode = 2
-    const val versionName = "0.2"
+    const val versionCode = 3
+    const val versionName = "0.3"
 
     const val applicationId = "com.henriquevieira.notes"
 
