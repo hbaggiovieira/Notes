b7a7f64a980b5e9b43eff49c771d7978b8984b3b
Henrique Vieira <hbaggiovieira@gmail.com>
2022-12-30 14:33:11 -0300

    Downgrade minSdkVersion

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,13 +1,13 @@
 
 object Config {
-    const val minSdkVersion = 24
+    const val minSdkVersion = 21
     const val targetSdkVersion = 33
     const val compileSdkVersion = 33
 
     const val buildToolsVersion = "33.0.1"
 
-    const val versionCode = 8
-    const val versionName = "1.1"
+    const val versionCode = 9
+    const val versionName = "1.2"
 
     const val applicationId = "com.henriquevieira.notes"
 
